201035bfd62520190d843ecc014fbb06960cb8e2
Marcelo Correa <marcelorcorrea@gmail.com>
2018-05-11 12:36:50 -0300

    Add ConnectionsRequest as a JsonSubType to be parsed

--- kalexa-model/src/main/kotlin/com/hp/kalexa/model/request/Request.kt
+++ kalexa-model/src/main/kotlin/com/hp/kalexa/model/request/Request.kt
@@ -14,6 +14,7 @@ import java.time.LocalDateTime
         JsonSubTypes.Type(value = SessionStartedRequest::class, name = "SessionStartedRequest"),
         JsonSubTypes.Type(value = SessionEndedRequest::class, name = "SessionEndedRequest"),
         JsonSubTypes.Type(value = ConnectionsResponseRequest::class, name = "Connections.Response"),
+        JsonSubTypes.Type(value = ConnectionsRequest::class, name = "Connections.Request"),
         JsonSubTypes.Type(value = IntentRequest::class, name = "IntentRequest"),
         JsonSubTypes.Type(value = ElementSelectedRequest::class, name = "Display.ElementSelected"),
         JsonSubTypes.Type(value = LaunchRequest::class, name = "LaunchRequest"))
