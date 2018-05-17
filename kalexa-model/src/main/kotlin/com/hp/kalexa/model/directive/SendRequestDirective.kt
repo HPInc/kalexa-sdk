e6e1e27eae3c2c9c1b112ef7e4465b91b5b686ba
Marcelo Correa <marcelorcorrea@gmail.com>
2018-05-17 08:27:22 -0300

    remove deprecated classes

--- kalexa-model/src/main/kotlin/com/hp/kalexa/model/directive/SendRequestDirective.kt
+++ kalexa-model/src/main/kotlin/com/hp/kalexa/model/directive/SendRequestDirective.kt
@@ -3,16 +3,12 @@ package com.hp.kalexa.model.directive
 import com.fasterxml.jackson.annotation.JsonTypeName
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.hp.kalexa.model.json.PayloadSerializer
+import com.hp.kalexa.model.payload.NameType
 import com.hp.kalexa.model.payload.Payload
 
 @JsonTypeName("Connections.SendRequest")
 class SendRequestDirective(
-        val name: Name,
+        val name: NameType,
         @JsonSerialize(using = PayloadSerializer::class)
         val payload: Payload<*>,
-        val token: String) : Directive() {
-
-    enum class Name {
-        PRINT, LOG, SCHEDULE
-    }
-}
+        val token: String) : Directive()
