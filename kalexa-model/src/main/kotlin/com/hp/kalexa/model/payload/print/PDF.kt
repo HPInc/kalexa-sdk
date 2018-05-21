42dba5d0585d4270e3f33ac181e15939367521e1
Marcelo Correa <marcelorcorrea@gmail.com>
2018-05-21 09:58:57 -0300

    Change AlexaResponseBuilder to Builder since this class is in AlexaResponse class. Add JvmOverloads annotation to action-pair classes in order to work with Java

--- kalexa-model/src/main/kotlin/com/hp/kalexa/model/payload/print/PDF.kt
+++ kalexa-model/src/main/kotlin/com/hp/kalexa/model/payload/print/PDF.kt
@@ -3,4 +3,8 @@ package com.hp.kalexa.model.payload.print
 import com.fasterxml.jackson.annotation.JsonTypeName
 
 @JsonTypeName("PDF")
-class PDF(title: String = "", description: String = "", url: String = "", version: String = "1.0") : Print.PrintType(title, description, url, version)
+class PDF @JvmOverloads constructor(
+        title: String = "",
+        description: String = "",
+        url: String = "",
+        version: String = "1.0") : Print.PrintType(title, description, url, version)
