d5bac662db704710c1e20b310043a06d5ed2c9ae
Marcelo Correa <marcelorcorre@hp.com>
2018-11-05 10:26:38 -0200

    Update Kotlin version. Move methods IntentUtils to corresponding classes.

--- kalexa-model/src/main/kotlin/com/hp/kalexa/model/Slot.kt
+++ kalexa-model/src/main/kotlin/com/hp/kalexa/model/Slot.kt
@@ -1,9 +1,23 @@
 package com.hp.kalexa.model
 
 import com.hp.kalexa.model.entitiyresolution.Resolutions
+import com.hp.kalexa.model.entitiyresolution.StatusCode
 
 data class Slot(val name: String,
                 val value: String? = null,
                 val confirmationStatus: SlotConfirmationStatus,
-                val resolutions: Resolutions? = null)
+                val resolutions: Resolutions? = null) {
+
+    /**
+     * Read a given slot and returns the value only if ER matches successfully
+     * @param Slot to be read
+     * @return value from the given slot
+     */
+    fun resolvedValue(): String? {
+        val resolution = resolutions?.resolutionsPerAuthority?.firstOrNull()
+        return resolution?.status?.code?.let { statusCode ->
+            if (statusCode == StatusCode.ER_SUCCESS_MATCH) resolution.values.firstOrNull()?.value?.name else null
+        }
+    }
+}
 
